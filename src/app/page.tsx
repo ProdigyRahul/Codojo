285486f06ddccbf56ede1d48711e19ef27d63487
Rahul Mistry <rahulmistry.sde@gmail.com>
2024-11-24 23:57:55 +0530

    feat: Added Shadcn Components and Basic Prisma Model for User

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import { Button } from "@/components/ui/button";
+
 export default async function Home() {
-  return <h1 className="text-red-500">Hello World</h1>;
+  return <Button>Click Me</Button>;
 }
