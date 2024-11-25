a238761cff62c408192aba8bbc452b5e9ba36317
Rahul Mistry <rahulmistry.sde@gmail.com>
2024-11-25 10:52:35 +0530

    feat: Dashboard Skaleton Layout

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -1,13 +1,46 @@
 "use client";
-import { useUser } from "@clerk/nextjs";
+import useProject from "@/hooks/use-project";
+import { ExternalLink, Github } from "lucide-react";
+import Link from "next/link";
 
 const DashboardPage = () => {
-  const { user } = useUser();
-
+  const { project } = useProject();
   return (
     <div>
-      <div>{user?.firstName}</div>
-      <div>{user?.lastName}</div>
+      <div className="flex flex-wrap items-center justify-between gap-y-4">
+        {/* Github Link */}
+        <div className="w-fit rounded-md bg-primary px-4 py-3">
+          <div className="item-center flex">
+            <Github className="size-5 text-white" />
+            <div className="ml-2">
+              <div className="text-sm font-medium text-white">
+                This project is linked to{" "}
+                <Link
+                  href={project?.githubUrl ?? ""}
+                  className="inline-flex items-center text-white/80 hover:underline"
+                >
+                  {project?.githubUrl}
+                  <ExternalLink className="ml-1 size-4" />
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="h-4"></div>
+        {/* Team Members and Invite Button */}
+        <div className="flex items-center gap-4">
+          Team Members Invite Button Archive Button
+        </div>
+      </div>
+
+      <div className="mt-4">
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-5">
+          AskQuestionCard MeetingCard
+        </div>
+      </div>
+
+      <div className="mt-8">Commit Log</div>
     </div>
   );
 };
